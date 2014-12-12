89ac5aae624598a70aad2d852539964d5c09eb80
Guido Van Hoof <guido.van.hoof3@telenet.be>
2014-12-12 20:32:44 +0100

    added test for redirect after post

--- lists/tests.py
+++ lists/tests.py
@@ -25,14 +25,28 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         request.method = 'POST'
         request.POST["item_text"] = 'A new list item'
-        expected_html = render_to_string(
-            "home.html", {"new_item_text": "A new list item"}
-        )
 
         response = home_page(request)
 
-        self.assertIn('A new list item', response.content.decode())
-        self.assertEqual(response.content.decode(), expected_html)
+        self.assertEqual(Item.objects.count(), 1)
+        newItem = Item.objects.first()
+        self.assertEqual(newItem.text, 'A new list item')
+
+    def test_home_page_redirects_after_post(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST["item_text"] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response["location"], "/")
+
+    def testHomePageOnlySavesItemsWhenNecessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
+
 
 class ItemModelTest(TestCase):
 
