649921c9a117c194e7737463ee9f033ba6e95413
Guido Van Hoof <guido.van.hoof3@telenet.be>
2014-12-13 10:15:30 +0100

    show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -9,43 +9,51 @@ from lists.views import home_page
 # Create your tests here.
 class HomePageTest(TestCase):
 
+    def setUp(self):
+        self.request = HttpRequest()
+
     def test_root_url_resolves_to_home_page(self):
         found = resolve("/")
         self.assertEqual(found.func, home_page)
 
     def test_home_page_geeft_correct_html_terug(self):
-        request = HttpRequest()
         expected_html = render_to_string("home.html")
 
-        response = home_page(request)
+        response = home_page(self.request)
 
         self.assertEqual(response.content.decode(), expected_html)
 
     def test_home_page_can_save_a_post_request(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST["item_text"] = 'A new list item'
+        self.request.method = 'POST'
+        self.request.POST["item_text"] = 'A new list item'
 
-        response = home_page(request)
+        home_page(self.request)
 
         self.assertEqual(Item.objects.count(), 1)
         newItem = Item.objects.first()
         self.assertEqual(newItem.text, 'A new list item')
 
     def test_home_page_redirects_after_post(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST["item_text"] = 'A new list item'
+        self.request.method = 'POST'
+        self.request.POST["item_text"] = 'A new list item'
 
-        response = home_page(request)
+        response = home_page(self.request)
 
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response["location"], "/")
 
     def testHomePageOnlySavesItemsWhenNecessary(self):
-        request = HttpRequest()
-        home_page(request)
+        home_page(self.request)
         self.assertEqual(Item.objects.count(), 0)
+
+    def testHomePageDisplaysAllListItems(self):
+        Item.objects.create(text="itemey 1")
+        Item.objects.create(text="itemey 2")
+
+        response = home_page(self.request)
+
+        self.assertIn("itemey 1", response.content.decode())
+        self.assertIn("itemey 2", response.content.decode())
 
 
 class ItemModelTest(TestCase):
